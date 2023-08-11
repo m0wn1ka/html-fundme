dc851f6e9b2f419b73b42c60a02e83a487e16c3a
m0wn1ka <rambabu986688@gmail.com>
2023-08-11 14:30:01 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# html-fund-me-f23
+
+## requirements
+etherjs,testnet,testether,wallet(metamask)
+## about
+using this anyone can fund the given contract at address 0xbBeA80cCaFA80B1B89AAA6F35f015D2852A60EDc<br>
+but only a owner can withdraw all the funds
+<br>anyone can get the balance of it 
---
+++ constants.js
@@ -0,0 +1,87 @@
+export const contractAddress = "0xbBeA80cCaFA80B1B89AAA6F35f015D2852A60EDc"
+export const abi = 
+  [
+    {
+      "inputs": [],
+      "name": "NotOwner",
+      "type": "error"
+    },
+    {
+      "inputs": [],
+      "name": "fund",
+      "outputs": [],
+      "stateMutability": "payable",
+      "type": "function"
+    },
+    {
+      "stateMutability": "payable",
+      "type": "receive"
+    },
+    {
+      "inputs": [],
+      "name": "withdraw",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "stateMutability": "payable",
+      "type": "fallback"
+    },
+    {
+      "inputs": [],
+      "stateMutability": "nonpayable",
+      "type": "constructor"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "name": "addressToAmountFunded",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "name": "funders",
+      "outputs": [
+        {
+          "internalType": "address",
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "i_owner",
+      "outputs": [
+        {
+          "internalType": "address",
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+  ]
